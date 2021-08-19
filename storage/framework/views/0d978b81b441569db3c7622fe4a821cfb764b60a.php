6c11ef10a0148ebd817848701010a7ce129cde22
Loid Ajax <tseworada@gmail.com>
2021-08-19 03:21:48 +0100

    Add files via upload

---
+++ storage/framework/views/0d978b81b441569db3c7622fe4a821cfb764b60a.php
@@ -0,0 +1,11 @@
+<h2>Hello Admin,</h2><br>
+You received an email from : <?php echo e($name); ?><br>
+Here are the details:
+<b>Name:</b> <?php echo e($name); ?><br>
+<b>Email:</b> <?php echo e($email); ?><br>
+
+<b>Subject:</b> <?php echo e($subject); ?><br>
+<b>Message:</b> <?php echo e($user_message); ?><br>
+Thank You
+
+<?php /**PATH C:\Program Files\Ampps\www\heroku\laravel_heroku\resources\views/mail.blade.php ENDPATH**/ ?>
